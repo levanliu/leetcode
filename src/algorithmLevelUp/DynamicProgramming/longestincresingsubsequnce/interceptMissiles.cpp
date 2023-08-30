dfd35ed0564d0df4f952d170fe10fde721c08c5a
levanliu <839119912@qq.com>
2023-08-30 08:55:17 +0800

    update solutions

---
+++ src/algorithmLevelUp/DynamicProgramming/longestincresingsubsequnce/interceptMissiles.cpp
@@ -0,0 +1,34 @@
+//acwing 1010
+#include<bits/stdc++.h>
+using namespace std;
+
+//greedy algorithm
+const int N = 10010;
+int n;
+int q[N],f[N];
+int g[N]; //存储每个序列末尾的数，该数组是一个单调递增的序列。可证明贪心与最优解数目相同 反链定理
+int main()
+{
+    while(cin >> q[n])n++;
+    int res = 0;
+    for(int i=n;i;i--){
+        f[i] = 1;
+        for(int j=n;j>i;j--){
+            if(q[i] > q[j]){
+                f[i] = max(f[i],f[j]+1);
+            }
+        }
+        res = max(f[i],q[i]);
+    }
+
+    int cnt =0;
+    for(int i=0;i<n;i++)
+    {
+        int k = 0;
+        while(k<cnt && g[k] < q[i]) k++; //遍历g数组，查找目标序列处。
+        g[k] = q[i]; //更新某个序列的末尾值,or k==cnt 放入新开序列之中
+        if(k>=cnt) cnt++;//新开序列
+    }
+    cout << cnt << endl;
+
+}
---
+++ src/algorithmLevelUp/DynamicProgramming/longestincresingsubsequnce/missileDefenceSystem.cpp
@@ -0,0 +1,57 @@
+// acwing 187
+//  3 2 5 4 1，target:多少个上升or下降子序列
+// 决策：是放到上升还是下降，无法优化，只能暴力搜索 dfs求最小步数（记忆一个全局最小值，不断更新）or（迭代加深）
+#include <bits/stdc++.h>
+using namespace std;
+
+const int N = 55;
+int n, ans;
+int q[N];
+int up[N], down[N];
+
+void dfs(int u, int su, int sd)
+{
+    if (su + sd >= ans)
+        return;
+    if (u == n)
+    {
+        ans = su + sd;
+        return;
+    }
+    // 情况一：将当前数放在上升子序列中
+    int k = 0;
+    while (k < su && up[k] >= q[u])
+        k++;
+    int t = up[k];
+    up[k] = q[u];
+    if (k < su)
+        dfs(u + 1, su, sd);
+    else
+        dfs(u + 1, su + 1, sd);
+    up[k] = t;
+
+    // 情况二：将当前数放在下降子序列中
+    k = 0;
+    while (k < su && down[k] <= q[u])
+        k++;
+    t = down[k];
+    down[k] = q[u];
+    if (k < sd)
+        dfs(u + 1, su, sd);
+    else
+        dfs(u + 1, su, sd + 1);
+    down[k] = t;
+}
+
+int main()
+{
+    while (cin >> n, n)
+    {
+        for (int i = 0; i < n; i++)
+            cin >> q[i];
+
+        ans = n;
+        dfs(0, 0, 0);
+        cout << ans;
+    }
+}
