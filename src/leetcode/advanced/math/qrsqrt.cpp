18afd16eb8fca76e33cd415927470c5c65828c9f
levanliu <839119912@qq.com>
2023-10-26 07:40:39 +0000

    add qsqrt

--- src/leetcode/advanced/math/qrsqrt.cpp
+++ src/leetcode/advanced/math/qrsqrt.cpp
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h>
+#include <climits>
 using namespace std;
 
 float Q_rsqrt(float number) {
@@ -6,13 +7,15 @@ float Q_rsqrt(float number) {
   float x2, y;
   const float threehalfs = 1.5F;
 
-  x2 = number * 0.5F;
+  x2 = number * 0.5F; //target number;
   y = number;
 
   i = *(long *)&y;
   i = 0x5f3759df - (i >> 1);
   y = *(float *)&i;
-  y = y * (threehalfs - (x2 * y * y));
+  // f(y) = 1/y^2 -x
+  // f'(y) = -2/y^3
+  y = y * (threehalfs - (x2 * y * y)); // yn+1 = yn *(1.5 - (x/2) * yn * yn)
   y = y * (threehalfs - (x2 * y * y));
 
   return y;
@@ -21,6 +24,7 @@ float Q_rsqrt(float number) {
 int main() {
   float a;
   cin >> a;
-  cout << Q_rsqrt(a);
+  cout << Q_rsqrt(a) << endl;
+  cout << sizeof(float) * CHAR_BIT << endl;
   return 0;
 }
