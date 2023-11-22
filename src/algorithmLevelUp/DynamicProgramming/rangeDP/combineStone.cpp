f9caeadb01331bce3298fe6727892e23e2dd90af
LevanLiu <839119912@qq.com>
2023-11-22 21:44:06 +0800

    20231028

---
+++ src/algorithmLevelUp/DynamicProgramming/rangeDP/combineStone.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+const int N = 307;
+int s[N];
+int f[N][N];
+
+int main() {
+  int n;
+  cin >> n;
+  for (int i = 1; i <= n; i++) cin >> s[i];
+  for (int i = 1; i <= n; i++) s[i] += s[i - 1];
+
+  for (int len = 2; len <= n; len++) {
+    for (int i = 1; i + len - 1 <= n; i++) {
+      int j = i + len - 1;
+
+      f[i][j] = 1e8;
+      for (int k = i; k < j; k++) {
+        f[i][j] = min(f[i][j], f[i][k] + f[k + 1][j] + s[j] - s[i - 1]);
+      }
+    }
+  }
+  cout << f[1][n];
+  return 0;
+}
