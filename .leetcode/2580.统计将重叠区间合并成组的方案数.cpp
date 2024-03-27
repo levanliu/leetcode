8497287617918e8c314edb7776f1d07f33fb8971
LevanLiu <839119912@qq.com>
2024-03-27 10:03:30 +0800

    add

---
+++ .leetcode/2580.统计将重叠区间合并成组的方案数.cpp
@@ -0,0 +1,28 @@
+#include <vector>
+#include <algorithm>
+
+class Solution
+{
+public:
+    static constexpr int mod = 1e9 + 7;
+    int countWays(vector<vector<int>> &ranges)
+    {
+        sort(ranges.begin(), ranges.end());
+        int n = ranges.size();
+        long long res = 1;
+        for (int i = 0; i < n;)
+        {
+            int j = i + 1;
+            int r = ranges[i][1];
+            while (j < n && r > ranges[j][0])
+            {
+                r = max(r, ranges[j][1]);
+                j++;
+
+                res = res * 2 % mod;
+            }
+            i = j;
+        }
+        return res;
+    }
+};
