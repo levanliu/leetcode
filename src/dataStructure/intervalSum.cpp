7c769c6bf7fc0f605d9b5efc0a42ffe6326e4dda
levanliu <839119912@qq.com>
2024-05-14 21:45:03 +0800

    add clang-format

--- src/dataStructure/intervalSum.cpp
+++ src/dataStructure/intervalSum.cpp
@@ -50,8 +50,8 @@
  * return l;
 */
 
-#include <iostream>
 #include <algorithm>
+#include <iostream>
 #include <vector>
 
 using namespace std;
@@ -64,60 +64,52 @@ int a[N], s[N];
 vector<int> alls;
 vector<PII> add, query;
 
-int find(int x)
-{
-    return lower_bound(alls.begin(), alls.end(), x) - alls.begin() + 1;
+int find(int x) {
+  return lower_bound(alls.begin(), alls.end(), x) - alls.begin() + 1;
 }
 
-vector<int>::iterator unique(vector<int> &a)
-{
-    int j = 0;
-    for (size_t i = 0; i < a.size(); i++)
-    {
-        if (!i || a[i] != a[i - 1])
-            a[j++] = a[i];
-    }
-    return a.begin() + j;
+vector<int>::iterator unique(vector<int>& a) {
+  int j = 0;
+  for (size_t i = 0; i < a.size(); i++) {
+    if (!i || a[i] != a[i - 1])
+      a[j++] = a[i];
+  }
+  return a.begin() + j;
 }
 
-int main()
-{
-    cin >> n >> m;
-    for (size_t i = 0; i < n; i++)
-    {
-        int x, c;
-        cin >> x >> c;
-        add.push_back({x, c});
+int main() {
+  cin >> n >> m;
+  for (size_t i = 0; i < n; i++) {
+    int x, c;
+    cin >> x >> c;
+    add.push_back({x, c});
 
-        alls.push_back(x);
-    }
+    alls.push_back(x);
+  }
 
-    for (size_t i = 0; i < m; i++)
-    {
-        int l, r;
-        cin >> l >> r;
-        query.push_back({l, r});
+  for (size_t i = 0; i < m; i++) {
+    int l, r;
+    cin >> l >> r;
+    query.push_back({l, r});
 
-        alls.push_back(l);
-        alls.push_back(r);
-    }
+    alls.push_back(l);
+    alls.push_back(r);
+  }
 
-    sort(alls.begin(), alls.end());
-    alls.erase(unique(alls), alls.end());
+  sort(alls.begin(), alls.end());
+  alls.erase(unique(alls), alls.end());
 
-    for (auto item : add)
-    {
-        int x = find(item.first);
-        a[x] += item.second;
-    }
+  for (auto item : add) {
+    int x = find(item.first);
+    a[x] += item.second;
+  }
 
-    for (int i = 1; i <= alls.size(); i++)
-        s[i] = s[i - 1] + a[i];
+  for (int i = 1; i <= alls.size(); i++)
+    s[i] = s[i - 1] + a[i];
 
-    for (auto item : query)
-    {
-        int l = find(item.first), r = find(item.second);
-        cout << s[r] - s[l - 1] << endl;
-    }
-    return 0;
+  for (auto item : query) {
+    int l = find(item.first), r = find(item.second);
+    cout << s[r] - s[l - 1] << endl;
+  }
+  return 0;
 }
